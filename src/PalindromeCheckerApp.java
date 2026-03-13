b3908cd842994b86780eafe07759fa2a6103dfa1
yuvashree5806-hub <yuvashree5806@gmail.com>
2026-03-13 20:01:29 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,8 @@
 public class PalindromeCheckerApp {
 
 
-        public static void main(String[] args) {
+       
 
-            System.out.println("=================================");
-            System.out.println("   Welcome to Palindrome Checker ");
-            System.out.println("=================================");
-            System.out.println("This application checks whether a word is a palindrome.");
-
-        }
+        
 
 }
